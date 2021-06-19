ac3a9ddcb02944b182bf4f45951bfff97f590323
SShan2021 <ssm2223@columbia.edu>
2021-06-18 17:44:41 -0700

    initial commit
    the initial files (identify_cluster.R)

---
+++ R/identify_cluster.R
@@ -0,0 +1,74 @@
+#' @title identify_cluster
+#'
+#' @description Goes through given top markers and extract those that match the
+#' cluster identifiers.
+#'
+#' @param topmarkers A dataset of topmarkers
+#'
+#' @return A data frame object that has all the markers and corresponding
+#' clusters and the cluster type for each gene.
+#'
+#' @examples
+#' \dontrun{
+#' output_table <- identify_cluster(topmarkers)
+#' }
+#'
+#' @export
+#' @importFrom dplyr "%>%"
+#'
+
+
+identify_cluster <- function(topmarkers){
+
+all.macrophages <- c("Adgre1", "Csf1r", "Fcgr1", "CD64", "Cd68", "Cd14", "Mafb", "Ly6c2")
+reslike.macrophages <- c("Csf1r", "Pf4", "Txnip", "F13a1", "Sepp1", "Lyve1", "Timp2", "Lyz2", "Folr2", "Ccl9", "Gas6", "CD206", "Mrc1")
+inflammatory.macrophages <- c("Cxcl2", "Cd14", "Cebpb", "Nlrp3", "Il1b", "Nfkbiz", "Egr1", "Zpf36", "Ccl2", "Tnf", "Ier3", "Ccl3", "Tlr2", "Nfkbid")
+trem2high.macrophages <- c("Trem2", "Cd9", "Ctsd", "Spp1", "Lgals3", "Atox1", "Ctsb", "Ctsz")
+ifnic.macrophages <- c("Isg15", "Irf7")
+monocyte <- c("F10", "Lilrb4a", "Ly6c2", "Ccr2", "Csf1r")
+modc.macrophages <- c("Cd209a", "Flt3", "Itgb7", "Napsa", "Ifi30", "Syngr2", "Cd74", "H2-Eb1")
+cDC1 <- c("Wdfy4", "Cd24", "Cd74")
+matureDC <- c("Ccr7", "Fscn1")
+t.cell <- c("Lck", "Cd3d", "Rag1", "Cd8a")
+cxcr6.t.cell <- c("Cxcr6", "Il7r", "Icos", "Cd3g")
+cd8.t.cell <- c("Cd8b1", "Cd3g", "Nkg7", "Cd8a")
+b.cells <- c("Cd79a", "Mzb1", "Cd79b", "Ly6d", "Ebf1", "Plac8")
+mast.cells <- c("Furin", "Il1rl1", "Calca")
+granulocytes <- c("Ngp", "Camp", "S100A8", "S100A9")
+nk.cells <- c("Klrb1c", "Gzma", "Klrc1", "Ncr1")
+cell.cycle.related <- c("Ccna2", "Cdk1", "Cdk4")
+smc.cells <- c("Acta2", "Cnn1", "Tagln", "Myh11")
+
+clustermarkers_list <- c(all.macrophages, reslike.macrophages, inflammatory.macrophages, trem2high.macrophages,
+                         ifnic.macrophages, monocyte, modc.macrophages, cDC1, matureDC, t.cell, cxcr6.t.cell, cd8.t.cell,
+                         b.cells, mast.cells, granulocytes, nk.cells, cell.cycle.related, smc.cells)
+
+clustermarkers <- as.data.frame(cbind.na(all.macrophages, reslike.macrophages, inflammatory.macrophages, trem2high.macrophages,
+                                         ifnic.macrophages, monocyte, modc.macrophages, cDC1, matureDC, t.cell, cxcr6.t.cell,
+                                         cd8.t.cell,b.cells, mast.cells, granulocytes, nk.cells, cell.cycle.related, smc.cells))
+
+datalist = list()
+for(i in 1:length(clustermarkers_list)){
+  datalist[[i]] <- topmarkers[topmarkers$gene == clustermarkers_list[i],]
+}
+
+merged_dataframe = do.call(rbind, datalist)
+
+merged_dataframe$celltype <- "other"
+
+for(i in 1:length(colnames(clustermarkers))){
+  for(j in 1:length(na.omit(clustermarkers[,i]))){
+    for(k in 1:length(merged_dataframe$gene)){
+      if(merged_dataframe$gene[k] == clustermarkers[j,i]){
+        merged_dataframe$celltype[k] <- colnames(clustermarkers)[i]
+      }
+    }
+  }
+
+}
+
+rownames(merged_dataframe) <- 1:length(merged_dataframe$gene)
+
+return(merged_dataframe)
+
+}
